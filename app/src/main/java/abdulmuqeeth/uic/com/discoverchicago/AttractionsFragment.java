b610e2859643e73579af37733509415019fece93
abdulmuqeethmohammed <amoham62@uic.edu>
2018-03-23 18:23:25 -0500

    Adding Attractions Fragment

---
+++ app/src/main/java/abdulmuqeeth/uic/com/discoverchicago/AttractionsFragment.java
@@ -0,0 +1,46 @@
+package abdulmuqeeth.uic.com.discoverchicago;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AttractionsFragment extends ListFragment {
+
+
+    public interface ListSelectionListener{
+        public void onListSelection(int index);
+    }
+
+    private ListSelectionListener mListener = null;
+
+    @Override
+    public void onAttach(Context activity) {
+        super.onAttach(activity);
+        try{
+            mListener = (ListSelectionListener) activity;
+        }catch (ClassCastException e){
+            throw new ClassCastException(activity.toString() + "must implement ListSelectionListener");
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+}
